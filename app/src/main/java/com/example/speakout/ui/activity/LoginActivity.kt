f6dd648a970bfee3528900c4b1943bf3267aebfd
Bernardo Júnior <bernardodemsj@gmail.com>
2020-01-24 18:24:19 -0300

    android studio initial files

---
+++ app/src/main/java/com/example/speakout/ui/activity/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.speakout.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
