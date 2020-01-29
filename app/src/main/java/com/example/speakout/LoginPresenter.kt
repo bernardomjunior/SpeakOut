c061cabd2d4ee424ae354da50e441aed50934f3a
Bernardo Júnior <bernardodemsj@gmail.com>
2020-01-29 15:21:20 -0300

    fix LoginPresenter format

--- app/src/main/java/com/example/speakout/LoginPresenter.kt
+++ app/src/main/java/com/example/speakout/LoginPresenter.kt
@@ -4,14 +4,23 @@ import android.util.Patterns
 
 class LoginPresenter(
     private val loginView: LoginContract.View
-): LoginContract.Presenter, LoginContract.Callback {
+) : LoginContract.Presenter, LoginContract.Callback {
 
     private val repository = LoginRepository(this)
 
     override fun login(email: String, password: String) {
-        if (isEmpty(email))             return loginView.notifyUser(R.string.empty_email, R.id.edittext_email_activity_login)
-        else if (isEmpty(password))     return loginView.notifyUser(R.string.empty_password, R.id.edittext_password_activity_login)
-        else if (!isEmailValid(email))  return loginView.notifyUser(R.string.invalid_email_format, R.id.edittext_email_activity_login)
+        if (isEmpty(email)) return loginView.notifyUser(
+            R.string.empty_email,
+            R.id.edittext_email_activity_login
+        )
+        else if (isEmpty(password)) return loginView.notifyUser(
+            R.string.empty_password,
+            R.id.edittext_password_activity_login
+        )
+        else if (!isEmailValid(email)) return loginView.notifyUser(
+            R.string.invalid_email_format,
+            R.id.edittext_email_activity_login
+        )
         repository.login(email, password)
     }
 
@@ -19,7 +28,7 @@ class LoginPresenter(
         return Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
-    private fun isEmpty(string: CharSequence): Boolean{
+    private fun isEmpty(string: CharSequence): Boolean {
         return string.isBlank()
     }
 
